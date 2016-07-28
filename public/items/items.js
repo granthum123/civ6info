20a26988f0a8d7f1313af13a3fb49407d21630ce
granthum <granthum@live.unc.edu>
2016-07-28 00:17:55 +0000

    fixed items title

--- public/items/items.js
+++ public/items/items.js
@@ -1,6 +1,7 @@
 angular.module('appControllers').controller('itemsCtrl', ['$scope', '$http', '$routeParams',function($scope, $http, $routeParams) {
     
 	$http.get('https://api-civ6.rhcloud.com/' + $routeParams.category).success(function(response){
+	    $scope.category = $routeParams.category.charAt(0).toUpperCase() + $routeParams.category.slice(1);
 		console.log("I got the data I requested");
 		console.log(response);
 		$scope.items = response.response;
