200b9534a4c5da4806825761df353ccfd6da456d
granthum <granthum@live.unc.edu>
2016-07-29 02:30:41 +0000

    added propeller ads tag

--- public/items/items.js
+++ public/items/items.js
@@ -7,3 +7,15 @@ angular.module('appControllers').controller('itemsCtrl', ['$scope', '$http', '$r
 		$scope.items = response.response;
 	})
 }]);
+angular.module('appControllers').controller('itemDetailsCtrl', ['$scope', '$http', "$routeParams", function($scope, $http, $routeParams){
+	//Get URL Param (replace later with scoped civname)
+	$http.get('https://api-civ6.rhcloud.com/items/' + $routeParams.name).success(function(response){
+		console.log(response);
+		console.log("I got the data I requested");
+		$scope.item = response.response[0];
+		$http.get('https://api-civ6.rhcloud.com/civs/' + $scope.civ.id + "/items").success(function(response2){
+			console.log(response2);
+			$scope.items = response2.response;
+		})
+	})
+}]);
